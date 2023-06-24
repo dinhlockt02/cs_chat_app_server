9daf8915baaac2f2620b86931e38ace40224e959
dinhlockt02 <dinhlockt02@gmail.com>
2023-06-24 19:49:14 +0700

    Add global socket

--- route/v1/internal/websocket.go
+++ route/v1/internal/websocket.go
@@ -1,30 +1,34 @@
 package routeinternal
 
 import (
+	"context"
+	"cs_chat_app_server/common"
 	"cs_chat_app_server/components/appcontext"
 	authmiddleware "cs_chat_app_server/middleware/authenticate"
-	"net/http"
-
+	gchatskt "cs_chat_app_server/modules/group_chat/transport/socket"
 	"github.com/gin-gonic/gin"
+	"github.com/gobwas/ws"
 )
 
 func InitSocketRoute(g *gin.RouterGroup, appCtx appcontext.AppContext) {
-	g.GET("/ws/chat", authmiddleware.Authentication(appCtx), func(c *gin.Context) {
+	g.GET("/ws", authmiddleware.Authentication(appCtx), websocketChatHandler(appCtx))
 
-		c.JSON(http.StatusMovedPermanently, gin.H{"data": "Move to /v1/friend/{{FRIEND_ID}}/chat/ws"})
-		return
+}
 
-		//conn, _, _, err := ws.UpgradeHTTP(c.Request, c.Writer)
-		//if err != nil {
-		//	panic(common.ErrInternal(err))
-		//}
-		//u, _ := c.Get(common.CurrentUser)
-		//requester, _ := u.(common.Requester)
-		//err = appCtx.Socket().AddConn(requester.GetId(), conn)
-		//if err != nil {
-		//	panic(common.ErrInternal(err))
-		//}
-		//appCtx.Socket().Receive(conn, c, pchatskt.SendMessageHandler(appCtx))
-	})
+func websocketChatHandler(appCtx appcontext.AppContext) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		conn, _, _, err := ws.UpgradeHTTP(c.Request, c.Writer)
+		if err != nil {
+			panic(common.ErrInternal(err))
+		}
+		u, _ := c.Get(common.CurrentUser)
 
+		requester, _ := u.(common.Requester)
+		err = appCtx.Socket().AddConn(requester.GetId(), conn)
+		if err != nil {
+			panic(common.ErrInternal(err))
+		}
+
+		appCtx.Socket().Receive(conn, context.Background(), gchatskt.SendMessageHandler(appCtx))
+	}
 }
