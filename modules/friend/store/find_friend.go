098e98f66858b821ac46417702a7c5054f91e8cb
dinhlockt02 <dinhlockt02@gmail.com>
2023-06-25 16:15:04 +0700

    Update sort order

--- modules/friend/store/find_friend.go
+++ modules/friend/store/find_friend.go
@@ -4,13 +4,18 @@ import (
 	"context"
 	"cs_chat_app_server/common"
 	friendmodel "cs_chat_app_server/modules/friend/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 // FindFriend is a method for finding user by filter
 // and store data into a slice of FriendUser struct
 func (s *mongoStore) FindFriend(ctx context.Context, filter map[string]interface{}) ([]friendmodel.FriendUser, error) {
 	var friends []friendmodel.FriendUser
-	cur, err := s.database.Collection(friendmodel.FriendUser{}.CollectionName()).Find(ctx, filter)
+
+	// Hardcoded sort order
+	opts := options.Find().SetSort(bson.D{{"name", 1}})
+	cur, err := s.database.Collection(friendmodel.FriendUser{}.CollectionName()).Find(ctx, filter, opts)
 	if err != nil {
 		return nil, common.ErrInternal(err)
 	}
--- modules/group/store/list.go
+++ modules/group/store/list.go
@@ -4,6 +4,8 @@ import (
 	"context"
 	"cs_chat_app_server/common"
 	groupmdl "cs_chat_app_server/modules/group/model"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 func (s *mongoStore) List(
@@ -11,8 +13,10 @@ func (s *mongoStore) List(
 	filter map[string]interface{},
 ) ([]groupmdl.Group, error) {
 
+	// Hardcoded sort order
+	opts := options.Find().SetSort(bson.D{{"latest_message.created_at", -1}})
 	cursor, err := s.database.Collection(groupmdl.Group{}.CollectionName()).
-		Find(ctx, filter)
+		Find(ctx, filter, opts)
 	if err != nil {
 		return nil, common.ErrInternal(err)
 	}
